befda3aba93bd316ecabd873b4ed84be9f9ba64e
MegaIDK <joaorevil@live.com>
2026-04-21 04:08:43 -0300

    base mapa: botão mapnode + mapmanager

---
+++ JogoDaTribo_Project/Assets/Scripts/MapManager.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+	private static MapManager _Instance;
+	public static MapManager Instance
+	{
+		get
+		{
+			if (!_Instance)
+			{
+				_Instance = new GameObject().AddComponent<MapManager>();
+				_Instance.name = _Instance.GetType().ToString();
+				DontDestroyOnLoad(_Instance.gameObject);
+			}
+			return _Instance;
+		}
+	}
+
+	private MapNode currentMapNode;
+	private string currentMapNodeID;
+	private List<string> clearedNodes;
+
+
+    private void Awake()
+    {
+        clearedNodes = new List<string>();
+		currentMapNodeID = "Start";
+    }
+
+
+	public MapNode GetCurrentMapNode()
+	{
+		return currentMapNode;
+	}
+
+	public void SetCurrentMapNode(MapNode mapNode)
+	{
+		currentMapNode = mapNode;
+	}
+
+	public string GetCurrentMapNodeID()
+	{
+		return currentMapNodeID;
+	}
+
+	public void SetCurrentMapNodeID(string ID)
+	{
+		currentMapNodeID = ID;
+	}
+
+	public List<string> GetClearedNodes()
+	{
+		return clearedNodes;
+	}
+
+	public void AddClearedNode(string nodeID)
+	{
+		clearedNodes.Add(nodeID);
+	}
+	
+	//funções de troca de cena
+	//fade in fade out?? overlay no canvas, dontdestroyonload
+}
